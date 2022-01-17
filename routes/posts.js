a2d9bdfc1a559bde401ee0ead8b7cc701f63542b
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 22:47:11 -0500

    Definiendo rutas en routes/post

--- routes/posts.js
+++ routes/posts.js
@@ -2,4 +2,11 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts');
 
+router.post('/products', controller.create);
+router.get('/products', controller.getAll);
+router.get('/products/:id', controller.getById);
+router.patch('/products/:id', controller.patchById);
+router.put('/products/:id', controller.updateById);
+router.delete('/products/:id', postsController.updateById);
+
 module.exports = router;
