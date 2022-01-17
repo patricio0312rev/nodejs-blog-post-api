298824af6849894cc7471639401cbd2d2b463ace
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 22:53:30 -0500

    Definiendo metodo get_handler

--- controllers/posts.js
+++ controllers/posts.js
@@ -21,3 +21,55 @@ function post_handler(req, res) {
             });
         });
 };
+
+function get_handler(req, res) {
+    const id = req.params.id;
+    if (id) {
+        Posts.findByPk(id)
+            .then(data => {
+                if (data)
+                    res.status(200).send(data);
+                else
+                    res.status(404).send("ID not found");
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "ID not found"
+                });
+            });
+    } else {
+        const author = req.query.author;
+        try {
+            var hasPublished = req.query.isPublished && (req.query.isPublished === 'true' || req.query.isPublished === 'false') ? true : false;
+            var isPublished = (req.query.isPublished && req.query.isPublished === 'true') ? true : false;
+        } catch (err) { };
+        var hasqry = false;
+
+        var jsonData = {};
+        var whereJson = {};
+
+        if (author) {
+            jsonData['author'] = author;
+            hasqry = true;
+        }
+        if (hasPublished) {
+            jsonData['isPublished'] = isPublished;
+            hasqry = true;
+        }
+        if (hasqry) {
+            //qryStr = '{ "where": { ' + qryStr + ' } }';
+            whereJson['where'] = jsonData;
+        } else {
+            qryStr = '{}';
+        }
+        Posts.findAll(whereJson)
+            .then(data => {
+                res.send(data);
+            })
+            .catch(err => {
+                res.status(404).send({
+                    message: "No Records Found"
+                });
+            });
+    }
+};
