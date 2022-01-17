1617345478f782ee682972c2b1ef9772f6e6fa81
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 22:53:02 -0500

    Creando funcion post_handler

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,23 @@
 const Posts = require('../models/posts');
+const { Op } = require('sequelize');
+
+module.exports = {
+    post_api: post_handler,
+    get_api: get_handler
+}
+
+function post_handler(req, res) {
+    let tmpdata = req.body;
+    if (tmpdata.isPublished) {
+        tmpdata.publishedDate = new Date().getTime();
+    }
+    Posts.create(tmpdata)
+        .then(data => {
+            res.status(201).send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while creating the Reminder."
+            });
+        });
+};
