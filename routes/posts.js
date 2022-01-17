1a3e3d61365d69dcddb9aa3ff946cfbda46ce472
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 22:51:31 -0500

    Redefiniendo rutas en routes/post.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,11 +2,17 @@ const express = require('express');
 const router = express.Router();
 const postsController = require('../controllers/posts');
 
-router.post('/products', controller.create);
-router.get('/products', controller.getAll);
-router.get('/products/:id', controller.getById);
-router.patch('/products/:id', controller.patchById);
-router.put('/products/:id', controller.updateById);
-router.delete('/products/:id', postsController.updateById);
+router.get('/', postsController.get_api);
+router.get('/:id', postsController.get_api);
+router.post('/', postsController.post_api);
+router.patch('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.put('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
+router.delete('/:id', function (req, res, next) {
+    res.status(405).send('<p>API does not allow deleting or modifying reminders for any id value</p>');
+});
 
 module.exports = router;
